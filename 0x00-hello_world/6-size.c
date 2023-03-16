76fec0f7cc8ab42efc6bdea58565be3f0e832310
omarIdhmaid <o.idhmaid@gmail.com>
2023-03-16 10:44:11 -0700

    betty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
 /**
- *  *main - Entry point
- *   *Return: Always 0 (Success)
- *    */
-
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 		printf("Size of a char: %zu byte(s)\n", sizeof(char));
