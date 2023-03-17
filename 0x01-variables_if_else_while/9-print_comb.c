3b3c4aff5e680bafe35c74f9fba3301e6aa7c998
omarIdhmaid <o.idhmaid@gmail.com>
2023-03-17 15:07:02 -0700

    commit 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Explain: 'alphABET'
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num, nl, cm, sp;
+
+	nl = '\n';
+	cm = ',';
+	sp = ' ';
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(num);
+		putchar(cm);
+		putchar(sp);
+	}
+	putchar(nl);
+	return (0);
+}
