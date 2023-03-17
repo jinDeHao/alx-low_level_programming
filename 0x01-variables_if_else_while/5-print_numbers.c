cb1d6cf24d8160c85524dffa6c450948aec4b752
omarIdhmaid <o.idhmaid@gmail.com>
2023-03-17 09:37:55 -0700

    0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Explain: 'from 0 to 9'
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		printf("%d", num);
+	}
+	return (0);
+}
