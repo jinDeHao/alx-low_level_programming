a2b3534c620cea68b26523bf4c5d58a96999db6f
omarIdhmaid <o.idhmaid@gmail.com>
2023-03-17 09:48:26 -0700

    no char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Explain: 'alphABET'
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num, nl;
+
+	nl = '\n';
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(num);
+	}
+	putchar(nl);
+	return (0);
+}
