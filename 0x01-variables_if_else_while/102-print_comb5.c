454c1bcaab8ad14da4cb222eccd9e8dd30e992b4
omarIdhmaid <o.idhmaid@gmail.com>
2023-03-17 19:15:06 -0700

    plzzzzz

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,32 +6,26 @@
  */
 int main(void)
 {
-	int n1, n2, n3, n4, cm, nl, sp;
+	int n1, n2, cm, nl, sp;
 
 	sp = ' ';
 	cm = ',';
 	nl = '\n';
-	for (n1 = 48; n1 <= 57; n1++)
+	for (n1 = 0; n1 < 100; n1++)
 	{
-		for (n2 = 48; n2 <= 57; n2++)
+		for (n2 = 0; n2 < 100; n2++)
 		{
-			for (n3 = 48; n3 <= 57; n3++)
+			if (n1 != n2)
 			{
-				for (n4 =  48; n4 <= 57; n4++)
+				putchar((n1 / 10) + '0');
+				putchar((n1 % 10) + '0');
+				putchar(sp);
+				putchar((n2 / 10) + '0');
+				putchar((n2 % 10) + '0');
+				if (n1 != 98 || n2 != 99)
 				{
-					if (n1 <= n3 && n2 < n4)
-					{
-					putchar(n1);
-					putchar(n2);
+					putchar(cm);
 					putchar(sp);
-					putchar(n3);
-					putchar(n4);
-					if (n1 != 57 || n2 != 56)
-					{
-						putchar(cm);
-						putchar(sp);
-					}
-					}
 				}
 			}
 		}
