9d2ba374eaeaa2ed9b7d2c7657b5d78d6cd20aa1
omarIdhmaid <o.idhmaid@gmail.com>
2023-03-21 11:04:42 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,7 +11,7 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar(43);
 		return (1);
 	}
 	else if (n == 0)
@@ -19,9 +19,9 @@ int print_sign(int n)
 		_putchar(48);
 		return (0);
 	}
-	else if (n < 0)
+	else (n < 0)
 	{
-		_putchar('-');
+		_putchar(45);
 		return (-1);
 	}
 	_putchar('\n');
