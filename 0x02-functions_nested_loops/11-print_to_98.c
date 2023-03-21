0a88370ecdc0fbab25e498a277982adb9fdfbe69
omarIdhmaid <o.idhmaid@gmail.com>
2023-03-21 14:15:37 -0700

    98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,45 @@
+nclude "main.h"
+/**
+ *print_to_98 - Entry point
+ *@n: the absolute value of the integer.
+ *Explain: '98 Battery Street, the OG'
+ */
+void print_to_98(int n)
+{
+	int a, de, hn;
+
+	if (n <= 98)
+	{
+		for (a = n; a <= 98; a++)
+		{
+			de = a / 10;
+			if (a < 0)
+			{
+				_putchar(45);
+			}
+			if (de != 0)
+			{
+				_putchar(de + 48);
+			}
+			_putchar(a % 10 + 48);
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	else
+	{
+		for (a = n; a >= 98; a--)
+		{
+			hn = a / 100;
+			if (hn != 0)
+			{
+				_putchar(hn + 48);
+			}
+			_putchar(a / 10 + 48);
+			_putchar(a % 10 + 48);
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('\n');
+}
