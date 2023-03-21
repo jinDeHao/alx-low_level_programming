114c68e98a206747a3374f16ef781cdc5d97ad89
omarIdhmaid <o.idhmaid@gmail.com>
2023-03-21 09:57:22 -0700

    lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_islower - Entry point
+ *Explain: 'lowercase'
+ */
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
