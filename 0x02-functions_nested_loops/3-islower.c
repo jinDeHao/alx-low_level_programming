99a1d3d25ef0719f859ea00d314ecc38f9994f82
omarIdhmaid <o.idhmaid@gmail.com>
2023-03-21 10:02:31 -0700

    lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,10 @@
 #include "main.h"
 /**
  *_islower - Entry point
+ *@c: The integer to check
  *Explain: 'lowercase'
+ *Return: On success 1.
+ *On error, 0 is returned.
  */
 int _islower(int c)
 {
