2c3f15283d5c5347574ee58822ce782815ef2e83
omarIdhmaid <o.idhmaid@gmail.com>
2023-03-21 16:57:56 -0700

    Fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Explain: 'Fibonacci numbers'
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int sum, i;
+
+	sum = 1;
+	for (i = 0; i < 50; i++)
+	{
+		printf("%d, ", sum);
+		sum += sum;
+	}
+	printf("\n");
+}
