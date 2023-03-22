3a807cfa4469bd41ddc165023e962c417e52af7f
omarIdhmaid <o.idhmaid@gmail.com>
2023-03-21 17:46:23 -0700

    Fibo

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Explain: 'Abbaci'
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	unsigned long a, b, res, sum;
+
+	a = 0;
+	b = 1;
+	res = a + b;
+	sum = 0;
+	while (res < 4000000)
+	{
+		res = a + b;
+		sum += res;
+		a = b;
+		b = res;
+	}
+	printf("%d\n", sum);
+}
