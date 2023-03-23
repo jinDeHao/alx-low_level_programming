e7296362efa8a453ea1f4a9d180add8c5a2cd4fe
omarIdhmaid <o.idhmaid@gmail.com>
2023-03-23 06:40:56 -0700

    Returns 1 if c is a digit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - writes the character c to stdout
+ * @c: The character to check
+ * Return: On success 0.
+ */
+int mul(int a, int b)
+{
+	int mltpl;
+
+	mltpl = a * b;
+	return (mltpl);
+}
