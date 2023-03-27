b6f7aa885966eb31d368b4b55f5e70f3662c1aed
omarIdhmaid <o.idhmaid@gmail.com>
2023-03-26 22:53:49 -0700

    task0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * reset_to_98 - updates the value it points to to 98.
+ * @n: The character to reset
+ * Return: On success ptr.
+ */
+void reset_to_98(int *n)
+{
+	int *ptr;
+
+	ptr = &n;
+	return (*ptr);
+}
