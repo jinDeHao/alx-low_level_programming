b8a697f8453bc559a69000bafb1869113a3ef155
omarIdhmaid <o.idhmaid@gmail.com>
2023-03-27 07:24:16 -0700

    task7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * puts_half - writes the half of string
+ * @str: The character to prin
+ */
+void puts_half(char *str)
+{
+	int l, n;
+
+	l = 0;
+	while (*str != '\0')
+	{
+		str++;
+		l++;
+	}
+	str -= (l - 1) / 2;
+	for (n = (l - 1) / 2; n < l - 1; n++)
+	{
+		_putchar(*str);
+		str++
+	}
+	_putchar('\n');
+}
