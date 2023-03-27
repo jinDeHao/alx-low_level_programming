2aa22f054d21d7ca6a25d640fb1260ff4aa4bb7f
omarIdhmaid <o.idhmaid@gmail.com>
2023-03-27 01:58:55 -0700

    task6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,13 +5,17 @@
  */
 void puts2(char *str)
 {
+	int i;
+
+	i = 0;
 	while (*str != '\0')
 	{
-		if (str % 2 == 0)
+		if (i % 2 == 0)
 		{
 			_putchar(*str);
 		}
 		str++;
+		i++;
 	}
 	_putchar('\n');
 }
