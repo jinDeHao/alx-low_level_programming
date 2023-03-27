0e0c7201da875354cd366c9cc020e91222ec30ca
omarIdhmaid <o.idhmaid@gmail.com>
2023-03-27 08:17:03 -0700

    task7

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _putchar - writes the character c to stdout
+ * @a: The pointer
+ * @n: The variable
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d, ", a[i]);
+	}
+	printf("\n");
+}
