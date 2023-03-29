54a8f25932183dc3520c0ba3d394deb89d315e2a
omarIdhmaid <o.idhmaid@gmail.com>
2023-03-29 14:49:25 -0700

    task3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,13 +3,16 @@
  * _strcmp - compare string values
  * @s1: input value
  * @s2: input value
- * Return: s1[i] - s2[i]
+ * Return: s1 - s2.
  */
 int _strcmp(char *s1, char *s2)
 {
-	if (*s1 != '\0' && *s2 != '\0')
+	while (*s1 != '\0' && *s2 != '\0')
 	{
-		return (s1 - s2);
+		if (*s1 != *s2)
+			return (s1 - s2);
+		s1++;
+		s2++;
 	}
 	return (0);
 }
