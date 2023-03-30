65eedd6de7480fd4cb9d78b3b91e256844389bf7
omarIdhmaid <o.idhmaid@gmail.com>
2023-03-29 17:07:27 -0700

    task6

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * leet - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success c.
+ */
+char *leet(char *c)
+{
+	char num[] = "43071", let[] = "AaEeOoTtLl";
+	int i, l, j;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		l = 0;
+		j = 0;
+		while (let[l] != '\0')
+		{
+			if (c[i] == let[l])
+			{
+				c[i] = num[j];
+			}
+			j += l % 2;
+			l++
+		}
+	}
+	return (c);
+}
