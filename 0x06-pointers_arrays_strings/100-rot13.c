e8ee612d8e648eb8ba131711f5149a0a56bfacbb
omarIdhmaid <o.idhmaid@gmail.com>
2023-03-29 18:33:22 -0700

    task8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rot13 - the character c to stdout
+ * @c: The character to print
+ * Return: On success c.
+ */
+char *rot13(char *c)
+{
+	char let1[] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+	char let2[] = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
+	int i, j;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		j = 0;
+		while (let1[j] != '\0')
+		{
+			if (c[i] == let1[j])
+			{
+				c[i] = let2[j];
+			}
+			j++;
+		}
+	}
+	return (c);
+}
