bebc8b26236054b24bead715aba7c5ebbe7e20b4
omarIdhmaid <o.idhmaid@gmail.com>
2023-04-03 00:31:02 -0700

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strchr - start from character
+ * @s: string
+ * @c: character to start from
+ * return: NULL if not found
+ * return: s
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		i++;
+		s++;
+	}
+	*s = '\0';
+	s -= i;
+	return (0);
+}
