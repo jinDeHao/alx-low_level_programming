337ed787617c607560cd9e684c1cae6657d3c263
omarIdhmaid <o.idhmaid@gmail.com>
2023-04-05 00:25:33 -0700

    task2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,16 +6,9 @@
  */
 int _strlen_recursion(char *s)
 {
-	int len = 0;
-
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		len++;
-		_strlen_recursion(s + 1);
+		return (1);
 	}
-	else
-	{
-		len++;
-		return(len);
-	}
+	return (1 + _strlen_recursion(s + 1));
 }
