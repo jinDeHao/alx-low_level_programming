c15706689f316f3ca499dfa5986b008b1dfbd7e4
omarIdhmaid <o.idhmaid@gmail.com>
2023-04-05 05:11:43 -0700

    task100

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -11,6 +11,26 @@ int is_palindrome(char *s)
 }
 
 /**
+ * _strlen_recursion - returns the length of a string
+ * @s: pointer to string
+ * Return: Always len
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == 0)
+		return (0);
+
+	else
+	{
+		if (*s == '\0')
+		{
+			return (0);
+		}
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
+
+/**
  * omar_palindrome - if a string is a palindrome
  * @s: string
  * @a: difference
