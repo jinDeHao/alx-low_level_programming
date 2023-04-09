087b697c756a9ca1484256d2a2409f7b07687efe
omarIdhmaid <o.idhmaid@gmail.com>
2023-04-09 14:44:55 -0700

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Explain: 'program name'
+ * @argc: the num of arguments
+ * @argv: pointer to array of strings
+ * Return: Always 0 (Success)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+So far, all the programs we have written can be run with a single command. For example, if we compile an executable called myprog, we can run it from within the same directory with the following command at the GNU/Linux command line   C - argc, argv
