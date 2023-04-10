73b084b35a6a09d8891ff88de296d8c2c569ab2d
omarIdhmaid <o.idhmaid@gmail.com>
2023-04-09 18:47:25 -0700

    adtask

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ * @argc: num of argument
+ * @argv: pointer to array of string
+ * Return: Always 0 (Success)
+ * on error 1 returned
+ */
+int main(int argc, char *argv[])
+{
+	int i, n = 0;
+	if (argc == 2)
+	{
+		i = atoi(argv[1]);
+		if (i >= 25)
+		{
+			n += i / 25;
+			i = i % 25;
+		}
+		if (i >= 10)
+		{
+			n += i / 10;
+			i = i % 10;
+		}
+		if (i >= 5)
+		{
+			n += i / 5;
+			i = i % 5;
+		}
+		if (i >= 2)
+		{
+			n += i / 2;
+			i = i % 2;
+		}
+		if (i >= 1)
+			n++;
+
+		printf("%d\n", n);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
