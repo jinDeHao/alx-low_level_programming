39cee84f059bb5b8a964e3773fbbb78c2e8e5a47
omarIdhmaid <o.idhmaid@gmail.com>
2023-04-11 16:32:15 -0700

    task6

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,84 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * strtow - splits a string into words
+ * @str: string
+ * Return buff
+ * on error NULL returned
+ */
+char **strtow(char *str)
+{
+	char **buff;
+	int i, j, len = 0, h = 0, w = 0, es = 0;
+
+	if (str == NULL || str == "")
+		return (NULL);
+
+	while (str[len] != '\0')
+	{
+		if (str[len] != ' ')
+		{	
+			if (str[len - 1] == ' ')
+			{	
+				h++;
+				es = 1;
+			}
+			if (es != 1 &&
+			(str[len + 1] == ' ' || str[len + 1] == '\0'))
+				h++;
+		}
+		len++;
+	}
+	len = 0;
+	buff = malloc(h * sizeof(char *));
+	if (buff == NULL)
+		return (NULL);
+
+	for (i = 0; i < h; i++)
+	{
+		w = 0;
+		es = 0;
+		while (str[len] != '\0')
+		{
+			if (str[len] != ' ')
+			{
+				w++;
+				es = 1;
+			}
+			if (str[len] == ' ' && es == 1)
+				break;
+			len++;
+		}
+		buff[i] = malloc((w + 1) * sizeof(char));
+		if (buff[i] == NULL)
+		{
+			for (; i >= 0; i--)
+				free(buff[i]);
+			free(buff);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < h; i++)
+	{
+		es = 0;
+		while (*str != '\0')
+		{	
+			if (*str != ' ')
+			{
+				*buff[i] = *str;
+				es = 1;
+				buff[i]++;
+			}
+			if (*str == ' ' && es == 1)
+			{
+				*buff[i] = '\0';
+				break;
+			}
+			str++;
+		}
+		if (i == h - 1)
+			*buff[i] = '\0';
+	}
+	return (buff);
+}
