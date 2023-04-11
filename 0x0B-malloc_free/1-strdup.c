91b529f7b78ce15bf256257514ea0e257b6fac4f
omarIdhmaid <o.idhmaid@gmail.com>
2023-04-11 04:01:41 -0700

    tas0

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,13 +12,16 @@ char *_strdup(char *str)
 	int i, l = 0;
 	char *buff;
 
-	while (str[l] > '\0')
-		l++;
-	
-	if (l == 0 || str == NULL)
+	if (str == NULL)
 		return (NULL);
 
+	while (str[l] >= '\0')
+		l++;
+
 	buff = malloc(l * sizeof(char));
+	if (buff == NULL)
+		return (NULL);
+
 	for (i = 0; str[i]; i++)
 		buff[i] = str[i];
 
