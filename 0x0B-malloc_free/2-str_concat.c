7858f76c063a075f6d2df3d081ecfc4a8043fa7c
omarIdhmaid <o.idhmaid@gmail.com>
2023-04-11 13:26:55 -0700

    task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,23 +13,22 @@ char *str_concat(char *s1, char *s2)
 	char *buff;
 	int i, j = 0, l = 0;
 
-	while (s1[j] != '\0')
-		j++;
-
-	while (s2[l] != '\0')
-		l++;
-
-	buff = malloc((l + j + 1) * sizeof(char));
 	if (s1 != NULL)
 	{
-		for (i = 0; i < j; i++)
-			buff[i] = s1[i];
+		while (s1[j] != '\0')
+			j++;
 	}
 	if (s2 != NULL)
 	{
+		while (s2[l] != '\0')
+			l++;
+	}
+	buff = malloc((l + j + 1) * sizeof(char));
+		for (i = 0; i < j; i++)
+			buff[i] = s1[i];
+
 		for (i = 0; i < l; i++)
 			buff[j + i] = s2[i];
-	}
 
 	buff[j + l] = '\0';
 	return (buff);
