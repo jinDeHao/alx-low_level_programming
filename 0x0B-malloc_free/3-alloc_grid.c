d2cb83410ddc0830fea8a3b447ea96e7e1ec44a1
omarIdhmaid <o.idhmaid@gmail.com>
2023-04-11 13:55:13 -0700

    task2

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * alloc_grid - 2 dimensional array of integers.
+ * @width: the width
+ * @height: the height
+ * Return: buff
+ * on error NULL returned
+ */
+int **alloc_grid(int width, int height)
+{
+	int *buff;
+	int i , j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	buff = malloc(width * height * sizeof(int));
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			buff[i][j] = 0;
+		}
+	}
+	return (buff);
+}
