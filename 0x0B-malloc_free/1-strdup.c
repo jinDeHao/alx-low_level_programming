61897a75296d8210f3ab20a2b961784fcbb5cac1
omarIdhmaid <o.idhmaid@gmail.com>
2023-04-11 03:30:35 -0700

    tas0

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - a copy of the string
+ * @str: the string given
+ * Return: buff
+ * on error NULL returned
+ */
+char *_strdup(char *str)
+{
+	int i, l = 0;
+	char buff;
+
+	while (*str)
+	{
+		l++;
+		str++;
+	}
+	str -= l;
+	if (l == 0)
+		return (0);
+
+	buff = malloc(l * sizeof(char));
+	for (i = 0; i < l; i++)
+		buff[i] = str[i];
+
+	return (buff);
+}
