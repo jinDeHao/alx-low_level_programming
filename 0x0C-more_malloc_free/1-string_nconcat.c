341bfedd29de6e93104e59d9c5b7e1db5a7d6e04
omarIdhmaid <o.idhmaid@gmail.com>
2023-04-13 16:35:51 -0700

    task1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,9 +14,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *buff;
 	unsigned int l1 = 0, l2 = 0, s = 0, i;
 
-	while (s1[l1] != '\0' && s1 != NULL)
+	while (s1[l1] && s1)
 		l1++;
-	while (s2[l2] != '\0' && s2 != NULL)
+	while (s2[l2] && s2)
 		l2++;
 	while (s < l1 + l2)
 	{
