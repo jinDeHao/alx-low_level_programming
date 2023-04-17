a873a748ced6b2324eb37f625ba32633adffefc2
omarIdhmaid <o.idhmaid@gmail.com>
2023-04-16 23:22:52 -0700

    task2

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+ * init_dog - initialize a variable of type struct dog
+ * @d: name of var
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - prints a struct dog
+ * @d: pointer to struct dog
+ */
+void print_dog(struct dog *d)
+{
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+	if (!d->age)
+		printf("Age: (nil)\n");
+	else
+		printf("Age: %f\n", d->age);
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
+}
