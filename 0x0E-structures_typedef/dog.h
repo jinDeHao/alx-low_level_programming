ded7c0b1760b10ede650eef8550fd1055fbecd38
omarIdhmaid <o.idhmaid@gmail.com>
2023-04-17 02:12:38 -0700

    task1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,15 +1,28 @@
 #ifndef DOG_H
 #define DOG_H
-
-struct dog{
+/**
+ * struct dog - dog's profil
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+/**
+ * dog_t - typedef for struct dog
+ */
 typedef struct dog dog_t;
+
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
+
 #endif
