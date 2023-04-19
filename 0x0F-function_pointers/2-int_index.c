2430975242370d69bac948912aa4a417d7352f69
omarIdhmaid <o.idhmaid@gmail.com>
2023-04-19 03:10:26 -0700

    task2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,8 +14,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[i]) != 0)
-			return (cmp(array[i]));
+		if ((*cmp)(array[i]) != 0)
+		{
+			return ((cmp)(array[i]));
+		}
 	}
 	return (-1);
 }
