7dd86f39c5dcca52fe49b7279186ef6b3fecb83a
omarIdhmaid <o.idhmaid@gmail.com>
2023-04-19 03:41:42 -0700

    taslk

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,7 +5,7 @@
  * @array: the array
  * @size: the number of elements in the array
  * @cmp: a pointer to the function to be used to compare values
- * Return: the index
+ * Return: i
  * on error -1 returned
  */
 int int_index(int *array, int size, int (*cmp)(int))
@@ -13,10 +13,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 
 	if (array == NULL || cmp == NULL || size <= 0)
-		return(-1);
+		return (-1);
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[i]))
+		if ((*cmp)(array[i]) != 0)
 			return (i);
 	}
 	return (-1);
