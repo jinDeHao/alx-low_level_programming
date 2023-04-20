ffa493443d9690a0b838c9838ad3cf0c005f5c1d
omarIdhmaid <o.idhmaid@gmail.com>
2023-04-19 19:59:15 -0700

    a

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - prints the opcodes 
+ * @argc: number of arg
+ * @argv: pointer to array of strings
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	char *array;
+
+	array = (char *)main;
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (i = 0; i < atoi(argv[1]); i++)
+	{
+		if (i == atoi(argv[1]) - 1)
+		{
+			printf("%02hhx\n", array[i]);
+			break;
+		}
+		printf("%02hhx ", array[i]);
+	}
+	return (0);
+}
