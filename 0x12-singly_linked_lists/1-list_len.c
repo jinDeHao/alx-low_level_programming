841e97f1399281fb0ff06910d5d865cb46933276
omarIdhmaid <o.idhmaid@gmail.com>
2023-04-27 10:39:38 -0700

    task1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len - the number of elements in a linked list_t list.
+ * @h: pointer to a node
+ * Return: n
+ */
+size_t list_len(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
