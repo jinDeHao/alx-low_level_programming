2a1792b529856b98c95e3909ea1a4125c9c3ab57
omarIdhmaid <o.idhmaid@gmail.com>
2023-04-27 15:14:50 -0700

    tas100k

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * first_of_all - Entry point
+ * Return: 0
+ */
+void first_of_all(void)
+{
+	char *txt = "You're beat! and yet, you must allow,\nI bore my house upon my back!\n";
+
+	printf("%s", txt);
+}
