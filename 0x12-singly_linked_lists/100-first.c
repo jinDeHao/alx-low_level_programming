64eaaf2572877b61c45ac562987376ac6898e3df
omarIdhmaid <o.idhmaid@gmail.com>
2023-04-27 15:18:20 -0700

    tas100k

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include "lists.h"
-void first_of_all(void) __attribute__ ((constructor));
 /**
  * first_of_all - Entry point
  * Return: 0
  */
 void first_of_all(void)
 {
-	char *txt = "You're beat! and yet, you must allow,\nI bore my house upon my back!\n";
+	char *txt1 = "You're beat! and yet, you must allow,\n";
+	char *txt2 = "I bore my house upon my back!\n";
 
-	printf("%s", txt);
+	printf("%s%s", txt1, txt2);
 }
