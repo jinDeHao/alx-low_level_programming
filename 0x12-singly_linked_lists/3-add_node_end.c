600003684d10fb5ad6d4050a98e4a73cc51c0f39
omarIdhmaid <o.idhmaid@gmail.com>
2023-04-27 13:35:40 -0700

    task2

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -27,7 +27,8 @@ list_t *add_node_end(list_t **head, const char *str)
 		*head = newest;
 		return (newest);
 	}
-	while (old)
+	if (old)
 		old = old->next;
 	old->next = newest;
+	return (newest);
 }
