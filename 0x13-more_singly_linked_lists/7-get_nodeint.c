14e0ebfd991350a91d032bccc21fb4daa73bfd39
omarIdhmaid <o.idhmaid@gmail.com>
2023-05-02 05:17:03 -0700

    task7 fix the error 6

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,10 +12,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
-	if (head == NULL)
-		return (NULL);
 	for (i = 0; i < index; i++)
+	{
+		if (head == NULL)
+			return (NULL);
 		head = head->next;
+	}
 	if (head == NULL)
 		return (NULL);
 	return (head);
