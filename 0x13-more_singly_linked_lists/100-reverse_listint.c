20c2c9ab086822dde26f93186305fc774193c508
omarIdhmaid <o.idhmaid@gmail.com>
2023-05-02 14:05:57 -0700

    task11 that reverse list

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * reverse_listint -  reverses alinked list.
+ * @head: point to pointer which point to linked list.
+ * Returns: the address of the list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	int i = 0;
+	listint_t *old, *current, *use;
+
+	use = *head;
+	while (use)
+	{
+		current = use;
+		use = use->next;
+		if (i == 0)
+			current->next = NULL;
+		else
+			current->next = old;
+		old = current;
+		i++;
+	}
+	*head = current;
+	return (current);
+}
