577c979d67d833a27d3848223db6a99ea38b01bc
omarIdhmaid <o.idhmaid@gmail.com>
2023-05-02 05:41:51 -0700

    task9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,8 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		if (use == NULL)
 			return (NULL);
-		if (i = idx - 1)
-			old = use;
+		old = use;
 		use = use->next;
 	}
 	if (use == NULL)
