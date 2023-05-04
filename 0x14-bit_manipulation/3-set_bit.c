0ad7b3c26ba8c077adc709afde07c2997a1ddf0a
omarIdhmaid <o.idhmaid@gmail.com>
2023-05-04 11:01:24 -0700

    task3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,6 +11,6 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (index >= 64)
 		return (-1);
 	if (!((n >> index) & 1))
-		n >> index = ~(n >> index);
+		*n >> index = ~(*n >> index);
 	return (1);
 }
