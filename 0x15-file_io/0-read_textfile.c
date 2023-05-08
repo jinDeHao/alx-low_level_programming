3e11fa6e4fb82c28032957e6bc7b3b3351113137
omarIdhmaid <o.idhmaid@gmail.com>
2023-05-08 15:08:04 -0700

    task0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,7 +14,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd;
 	char *buff;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
 	fd = open(*filename, O_RDONLY);
 	if (fd == -1)
