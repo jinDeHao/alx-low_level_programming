0549ed7cbc48bbdee29b2bc084a5ac539c769afe
omarIdhmaid <o.idhmaid@gmail.com>
2023-05-08 16:00:12 -0700

    task1 write 0600 instead of 600

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,11 +14,9 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	/**
-	 * if (!text_content)
-	 *	text_content = "\0";
-	 */
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 600);
+	if (!text_content)
+		text_content = "\0";
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (fd);
 	as_error = write(fd, text_content, strlen(text_content));
