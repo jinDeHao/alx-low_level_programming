172ae15f2322bad83a991f2845fb7a6216972bfb
omarIdhmaid <o.idhmaid@gmail.com>
2023-05-08 18:47:02 -0700

    task3 fix more than 1024 letters

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,41 +11,42 @@ char *mall_error(char *to_file);
  */
 int main(int argc, char *argv[])
 {
-	int fd, size_read, er;
+	int fd_from, fd_to, size_read, er_from, er_to, er;
 	char *buff;
 
 	arg_error(argc);
 	buff = mall_error(argv[2]);
-	fd = open(argv[1], O_RDONLY);
-	size_read = read(fd, buff, 1024);
-	if (fd == -1 || size_read == -1)
+	fd_from = open(argv[1], O_RDONLY);
+	size_read = read(fd_from, buff, 1024);
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	do {
+		if (fd_from == -1 || size_read == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			free(buff);
+			exit(98);
+		}
+		er = write(fd_to, buff, size_read);
+		if (er == -1 || fd_to == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			free(buff);
+			exit(99);
+		}
+		size_read = read(fd_from, buff, 1024);
+		fd_to = open(argv[2], O_WRONLY | O_APPEND);
+	} while (size_read >= 1);
+	er_from = close(fd_from);
+	if (er_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	er = close(fd);
-	if (er == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		free(buff);
 		exit(100);
 	}
-	fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd == -1)
+	er_to = close(fd_to);
+	if (er_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-	er = write(fd, buff, size_read);
-	if (er == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		free(buff);
-		exit(99);
-	}
-	er = close(fd);
-	if (er == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 		free(buff);
 		exit(100);
 	}
