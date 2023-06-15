6df17cf1ae397e72ae485452639b6d8eebe0844d
omarIdhmaid <o.idhmaid@gmail.com>
2023-06-15 11:17:39 +0100

    task0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,18 +7,20 @@
 */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t n = 0;
-    const dlistint_t *node = h;
+	size_t n = 0;
+	const dlistint_t *node = h;
 
-    /*to set the node to head
-    while (node->prev)
-        node = node->prev;*/
-    
-    while (node)
-    {
-        printf("%d\n", node->n);
-        n++;
-        node = node->next;
-    }
-    return (n);
+	/*
+	*to set the node to head
+	*while (node->prev)
+	*	node = node->prev;
+	*/
+
+	while (node)
+	{
+		printf("%d\n", node->n);
+		n++;
+		node = node->next;
+	}
+	return (n);
 }
