bfd5512e114f2c40f23de1b148477b464a0a73c5
omarIdhmaid <o.idhmaid@gmail.com>
2023-06-15 11:10:13 +0100

    task0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,9 +10,9 @@ size_t print_dlistint(const dlistint_t *h)
     size_t n = 0;
     const dlistint_t *node = h;
 
-    /*to set the node to head*/
+    /*to set the node to head
     while (node->prev)
-        node = node->prev;
+        node = node->prev;*/
     
     while (node)
     {
