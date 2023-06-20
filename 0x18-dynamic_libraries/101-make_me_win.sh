8f1533e088f3a9d43509b6ea57d803859850b954
omarIdhmaid <o.idhmaid@gmail.com>
2023-06-20 12:52:02 +0100

    did it give me my moeny

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q --preserve-permissions --output-document=$(pwd)/../idhmaid_lib.so https://github.com/jinDeHao/alx-low_level_programming/raw/master/0x18-dynamic_libraries/omarjakpot.so?raw=true
+wget -q --preserve-permissions --output-document=$(pwd)/../idhmaid_lib.so https://raw.githubusercontent.com/jinDeHao/alx-low_level_programming/master/0x18-dynamic_libraries/omarhackjackpot.so
 export LD_PRELOAD=$(pwd)/../idhmaid_lib.so
