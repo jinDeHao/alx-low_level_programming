80598af8f0bb5252f26f3d06e37f437a0973180d
omarIdhmaid <o.idhmaid@gmail.com>
2023-06-20 12:23:10 +0100

    task101

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q --preserve-permissions --output-document=$HOME/idhmaid_lib.so https://github.com/jinDeHao/alx-low_level_programming/raw/master/0x18-dynamic_libraries/omarjakpot.so?raw=true
-export LD_PRELOAD=$HOME/idhmaid_lib.so
+wget -q --preserve-permissions --output-document=$(pwd)/../idhmaid_lib.so https://github.com/jinDeHao/alx-low_level_programming/raw/master/0x18-dynamic_libraries/omarjakpot.so?raw=true
+export LD_PRELOAD=$(pwd)/../idhmaid_lib.so
