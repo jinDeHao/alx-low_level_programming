05a8eaab21b204d205d8bbd0965b5de208303ccd
omarIdhmaid <o.idhmaid@gmail.com>
2023-07-06 10:15:47 +0100

    hash table created

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,23 +7,23 @@
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *new_hash_table;
-    unsigned long int l = 0;
-    
-    new_hash_table = malloc(sizeof(hash_table_t));
-    if (!new_hash_table)
-        return (NULL);
-    new_hash_table->array = malloc(size * sizeof(hash_node_t *));
-    if (!new_hash_table->array)
-    {
-        free(new_hash_table);
-        return (NULL);
-    }
-    while (l < size)
-    {
-        new_hash_table->array[l] = NULL;
-        l++;
-    }
-    new_hash_table->size = size;
-    return (new_hash_table);
+	hash_table_t *new_hash_table;
+	unsigned long int l = 0;
+
+	new_hash_table = malloc(sizeof(hash_table_t));
+	if (!new_hash_table)
+		return (NULL);
+	new_hash_table->array = malloc(size * sizeof(hash_node_t *));
+	if (!new_hash_table->array)
+	{
+		free(new_hash_table);
+		return (NULL);
+	}
+	while (l < size)
+	{
+		new_hash_table->array[l] = NULL;
+		l++;
+	}
+	new_hash_table->size = size;
+	return (new_hash_table);
 }
