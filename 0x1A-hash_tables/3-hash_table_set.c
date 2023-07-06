e251f337acf3f805322fa90ed2c524d808599c75
omarIdhmaid <o.idhmaid@gmail.com>
2023-07-06 20:56:38 +0100

    task3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,35 +14,28 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!value || !key || *key == '\0')
 		return (0);
 	idx = key_index((const unsigned char *)key, ht->size);
-	if (add_node_hash(&(ht->array[idx]), key, value))
+	if (add_node_hash(&(ht->array[idx]), (char *)key, (char *)value))
 		return (1);
 	return (0);
 }
 
 /**
- * add_node - adds a new node at the beginning of a list_t list.
+ * add_node_hash - adds a new node at the beginning of a list_t list.
  * @head: pointer to pointer
  * @key: the new key
  * @value: the new value
  * Return: newest node
  */
-hash_node_t *add_node_hash(hash_node_t **head, const char *key, const char *value)
+hash_node_t *add_node_hash(hash_node_t **head, char *key, char *value)
 {
 	hash_node_t *new_hash;
-/**
-*	hash_node_t *tmp = *head;
-*
-*	while (tmp)
-*	{
-*		if (strcmp(tmp->key, key))
-*		{
-*			free(tmp->value);
-*			tmp->value = strdup(value);
-*			return (*head);
-*		}
-*		tmp = tmp->next;
-*	}
-*/
+
+	if ((*head)->key && !strcmp((*head)->key, key))
+	{
+		free((*head)->value);
+		(*head)->value = strdup(value);
+		return (*head);
+	}
 	new_hash = malloc(sizeof(hash_node_t));
 	if (!new_hash)
 		return (NULL);
