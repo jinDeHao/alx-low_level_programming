e1c0a0fb161a6c35bf43002698d1e492dd38daa6
omarIdhmaid <o.idhmaid@gmail.com>
2023-07-07 02:53:59 +0100

    delete hash table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+#include <stdio.h>
+
+void hash_table_delete(hash_table_t *ht)
+{
+	int i = 0;
+	hash_node_t *letsfree;
+
+	while (i < (int)ht->size)
+	{
+		while (ht->array[i])
+		{
+            letsfree = ht->array[i];
+			ht->array[i] = ht->array[i]->next;
+            free(letsfree->key);
+            free(letsfree->value);
+            free(letsfree);
+		}
+		i++;
+	}
+    if (ht->array)
+        free(ht->array);
+    if (ht)
+        free(ht);
+}
