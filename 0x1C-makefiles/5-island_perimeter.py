ec2c38cd32341c1fa115deca46d65e47c2ed54c4
omarIdhmaid <o.idhmaid@gmail.com>
2023-08-24 02:45:15 +0100

    island

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,6 +3,7 @@
 island_perimeter
 """
 
+
 def island_perimeter(grid):
     """
     count lake of island
@@ -20,3 +21,16 @@ def island_perimeter(grid):
                 if j + 1 < len(grid[i]) and grid[i][j + 1] == 0:
                     lake += 1
     return lake
+    # lakes = 0
+    # for i in range(len(grid)):
+    #     for j in range(len(grid[i])):
+    #         if grid[i][j] == 0:  # Found a lake
+    #             if i == 0 or grid[i - 1][j] == 0:  # Check top
+    #                 lakes += 1
+    #             if i == len(grid) - 1 or grid[i + 1][j] == 0:
+    #                 lakes += 1
+    #             if j == 0 or grid[i][j - 1] == 0:  # Check left
+    #                 lakes += 1
+    #             if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
+    #                 lakes += 1
+    # return lakes
