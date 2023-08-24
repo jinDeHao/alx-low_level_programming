dec84fa376c74e202f05d6ea32896e567313d436
omarIdhmaid <o.idhmaid@gmail.com>
2023-08-24 02:55:30 +0100

    boolean

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,26 +11,13 @@ def island_perimeter(grid):
     lake = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            if grid[i][j] == 1:
-                if i > 0 and grid[i - 1][j] == 0:
+            if grid[i][j]:
+                if i > 0 and not grid[i - 1][j]:
                     lake += 1
-                if i + 1 < len(grid) and grid[i + 1][j] == 0:
+                if i + 1 < len(grid) and not grid[i + 1][j]:
                     lake += 1
-                if j > 0 and grid[i][j - 1] == 0:
+                if j > 0 and not grid[i][j - 1]:
                     lake += 1
-                if j + 1 < len(grid[i]) and grid[i][j + 1] == 0:
+                if j + 1 < len(grid[i]) and not grid[i][j + 1]:
                     lake += 1
     return lake
-    # lakes = 0
-    # for i in range(len(grid)):
-    #     for j in range(len(grid[i])):
-    #         if grid[i][j] == 0:  # Found a lake
-    #             if i == 0 or grid[i - 1][j] == 0:  # Check top
-    #                 lakes += 1
-    #             if i == len(grid) - 1 or grid[i + 1][j] == 0:
-    #                 lakes += 1
-    #             if j == 0 or grid[i][j - 1] == 0:  # Check left
-    #                 lakes += 1
-    #             if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
-    #                 lakes += 1
-    # return lakes
