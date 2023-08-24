e1e7b1a4a99a9b15887a44ad43eec36b5e210963
omarIdhmaid <o.idhmaid@gmail.com>
2023-08-24 03:04:37 +0100

    no others

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -12,12 +12,19 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j]:
+                ch = 0
                 if i > 0 and not grid[i - 1][j]:
                     lake += 1
+                    ch += 1
                 if i + 1 < len(grid) and not grid[i + 1][j]:
                     lake += 1
+                    ch += 1
                 if j > 0 and not grid[i][j - 1]:
                     lake += 1
+                    ch += 1
                 if j + 1 < len(grid[i]) and not grid[i][j + 1]:
                     lake += 1
+                    ch += 1
+                if lake > 3 and ch >= 3:
+                    return lake
     return lake
