3f7822c96ac246713bd4eb55de044223f8bce6ab
omarIdhmaid <o.idhmaid@gmail.com>
2023-10-08 19:18:06 +0100

    binary search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,4 +5,8 @@
 
 int linear_search(int *array, size_t size, int value);
 
+int binary_search(int *array, size_t size, int value);
+void printarr(int *array, size_t start, size_t end);
+size_t binary_help(int *array, int value, size_t start, size_t end);
+
 #endif
