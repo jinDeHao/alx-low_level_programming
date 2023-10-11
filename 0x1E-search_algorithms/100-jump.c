4228ceb8fe360c29b8fc18a8505470d4fa0d8379
omarIdhmaid <o.idhmaid@gmail.com>
2023-10-11 22:38:01 +0100

    jumping algo

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,29 @@
+#include "search_algos.h"
+
+/**
+ * jump_search - shearch using the dictionary technic
+ * @array: the list
+ * @size: the size of the list
+ * @value: the value looking for
+ * Return: the index if success -1 when failed
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t m = (size_t)sqrt(size), i = 0;
+
+	while (array[i] < value && i < size)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		i += m;
+	}
+	printf("Value found between indexes [%ld] and [%ld]\n", i - m, i);
+	i -= m;
+	while (array[i] <= value && i < size)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,11 +2,14 @@
 #define SEARCH_ALGOS_H
 
 #include <stdio.h>
+#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
 
 int binary_search(int *array, size_t size, int value);
 void printarr(int *array, size_t start, size_t end);
 size_t binary_help(int *array, int value, size_t start, size_t end);
+
+int jump_search(int *array, size_t size, int value);
 
 #endif
